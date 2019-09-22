f3606d7200f255a510e2b88ab6c4c41411137a40
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2019-09-22 16:49:08 +0200

    Changed Clash Royale API to Clash Royale API

--- dist/index.js
+++ dist/index.js
@@ -19,32 +19,32 @@ var _url2 = _interopRequireDefault(_url);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-var port = process.env.COC_PORT || 8080;
-var token = process.env.COC_TOKEN;
+var port = process.env.CR_PORT || 8080;
+var token = process.env.CR_TOKEN;
 
 if (!token) {
-  console.log('Please specify a COC_TOKEN env variable');
-  process.exit(1);
+    console.log('Please specify a CR_TOKEN env variable');
+    process.exit(1);
 }
 
 app.use((0, _cors2.default)());
-app.use((0, _expressHttpProxy2.default)('https://api.clashofclans.com', {
-  filter: function filter(req, res) {
-    return req.method == 'GET';
-  },
-  forwardPath: function forwardPath(req, res) {
-    return _url2.default.parse(req.url).path;
-  },
-  decorateRequest: function decorateRequest(req) {
-    req.headers['Authorization'] = 'Bearer ' + token;
-    return req;
-  }
+app.use((0, _expressHttpProxy2.default)('https://api.clashroyale.com/v1/', {
+    filter: function filter(req, res) {
+        return req.method == 'GET';
+    },
+    proxyReqPathResolver: function proxyReqPathResolver(req, res) {
+        return _url2.default.parse(req.url).path;
+    },
+    proxyReqOptDecorator: function proxyReqOptDecorator(proxyReqOpts, srcReq) {
+        proxyReqOpts.headers['Authorization'] = 'Bearer ' + token;
+        return proxyReqOpts;
+    }
 }));
 
 app.listen(port, function (err) {
-  if (err) {
-    throw err;
-  }
+    if (err) {
+        throw err;
+    }
 
-  console.log('COC proxy listening on port ' + port);
+    console.log('CR proxy listening on port ' + port);
 });
